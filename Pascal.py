2ae23b5b132cc1bb6d52f72f9a7d9ea010373a1e
tjacobconley1 <tjacobconley1@gmail.com>
2021-07-22 06:00:40 +0000

    pascal with directions

--- Pascal.py
+++ Pascal.py
@@ -1,7 +1,34 @@
 # PASCALS TRIANGLE
 # Author: Tyler Conley
+
+# Pascal's triangle is a never ending equilateral triangle
+# of numbers that follow a rule of adding the two numbers
+# above to get the number below
+
+# The Binomial Theorem:
+# (a + b)^n = the summation from 0 to n as a polynomial
+# the coefficients of the polynomial will be the pascal triangle
+# so....
+#
+#         (x + y)^0 = 1
+#
+#  and
+#
+#         (x + y)^n = x^n + nxy + y^n
+#
+
+
+
 import numpy as np
 import math
+
+# array size scaler
+scale = int(input("how many iterations? ->"))
+
+# create blank array of size 'scale'
+pascalgrid = np.zeros((scale, scale))
+
+print(pascalgrid)
 
 i = 0
 
